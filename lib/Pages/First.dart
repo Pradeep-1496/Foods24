742fab9b05a9875702caa0823eaec534815321ec
pradeep-1496 <d23dit103@charusat.edu.in>
2025-03-17 22:50:42 +0530

    bottom overflow

--- lib/Pages/First.dart
+++ lib/Pages/First.dart
@@ -16,154 +16,150 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Logo
-                Image.asset('assets/logo.png', width: 200, height: 200),
-
-                // Title
-                const SizedBox(height: 20),
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-
-                // Subtitle
-                const SizedBox(height: 10),
-                Text(
-                  'Before Enjoying Foodmedia Services\nPlease Register First',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                  ),
-                ),
-
-                const SizedBox(height: 150),
-
-                // Gradient Create Account Button
-                Container(
-                  width: 250, // Set the width as per your requirement
-                  height: 56,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.orange, Colors.deepOrange],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      // ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 100.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/logo.png', width: 200, height: 200),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegistrationScreen(),
-                          ));
-                    },
-                    child: Text(
-                      'Create Account',
+                    const SizedBox(height: 10),
+                    Text(
+                      'Before Enjoying Foodmedia Services\nPlease Register First',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                        color: Colors.black54,
                       ),
                     ),
-                  ),
+                  ],
                 ),
-
-                const SizedBox(height: 20),
-
-                // Light Orange Login Button
-                ElevatedButton(
+              ),
+              Container(
+                width: 250,
+                height: 56,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.orange, Colors.deepOrange],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange.shade100,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    minimumSize: Size(250, 56), // Set the width here
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   onPressed: () {
                     Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Login(),
-                        ));
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegistrationScreen(),
+                      ),
+                    );
                   },
                   child: Text(
-                    'Login',
+                    'Create Account',
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
-                      color: Colors.deepOrange,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-
-                const SizedBox(height: 80),
-
-                // Terms and Conditions
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text:
-                            'By Logging In Or Registering, You Have Agreed To The ',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Terms And Conditions ',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.orange, // Orange color
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'And ',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Privacy Policy.',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.orange, // Orange color
-                        ),
-                      ),
-                    ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange.shade100,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  minimumSize: Size(250, 56),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  textAlign: TextAlign.center,
-                )
-              ],
-            ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 80),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          'By Logging In Or Registering, You Have Agreed To The ',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Terms And Conditions ',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'And ',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Privacy Policy.',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
         ),
       ),
